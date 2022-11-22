9700bd7bbf89d1a8a05f305f0e71f3d96232a7a3
athyaasaghi <asaghiathya@gmail.com>
2022-11-22 10:50:28 +0700

    responsive

--- coba.js
+++ coba.js
@@ -1,12 +1,15 @@
 const tulisan_tombol = document.querySelector('.tulisan_tombol');
 tulisan_tombol.addEventListener('mouseenter', function() {
-    tulisan_tombol.style.backgroundColor = 'grey';
+    tulisan_tombol.style.backgroundColor = '#ddd';
+    tulisan_tombol.style.transform = 'scale(1.2)'
+    tulisan_tombol.style.transition = '1s';
 });
 
 
 tulisan_tombol.addEventListener('mouseleave', function() {
-    tulisan_tombol.style.backgroundColor = 'rgba(197, 181, 181, 0.2)';
-    tulisan_tombol.style.boxShadow = '0 7px 3px rgba(0,0,0, 0.9)';
+    tulisan_tombol.style.backgroundColor = '#b8e0f5';
+    tulisan_tombol.style.boxShadow = '0 7px 6px rgb (220.220.220)';
+    tulisan_tombol.style.transform = 'scale(1)'
 });
 
 tulisan_tombol.addEventListener('click', function() {
